ca851b49cc690a8dfd92b10e04c5399e0522dc06
Kyle Rudderforth <kruddzz@gmail.com>
2020-07-03 02:15:02 -0500

    generateMarkdown.js filled in and linked

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,37 @@
 // function to generate markdown for README
+// figure out license badge.
+// Do I need a second badge specific to the repo
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `#
+  # Project Title : ${data.title}
 
+  ## Project Description: ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Credits](#credits)
+  * [Contributing](#contributing)
+  * [Test](#test)
+  * [Questions](#questions)
+  
+  ## Installation: ${data.install}
+
+  ## Usage: ${data.usage}
+
+  ##License: ${data.license}
+
+  ## Credits: ${data.credits}
+  
+  ## Contributors: ${data.contributors}
+
+  ## Test: ${data.test}
+
+  ## Questions:
+  ${data.question}
+  Email: ${data.email}
+  Git Hub Profile: https://github.com/${data.userName}
 `;
 }
 
